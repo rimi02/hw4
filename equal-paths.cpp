8fe06b2c553e1da52f19fc4f6b1cecd30e897b87
Uplabdhi Bhardwaj <rimbhard@usc.edu>
2022-11-08 02:48:24 +0000

    updating equal-paths.cpp and equal-paths.h

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,13 +1,30 @@
 #include "equal-paths.h"
 using namespace std;
 
-
-// You may add any prototypes of helper functions here
-
-
-bool equalPaths(Node * root)
-{
-    // Add your code below
-
+int getDepth(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int left = getDepth(root->left);
+    int right = getDepth(root->right);
+    if(left > right){
+        return left + 1;
+    }
+    return right + 1;
 }
 
+bool equalPaths(Node* root){
+    if(root == NULL){
+        return true;
+    }
+    if(root->left == NULL && root->right == NULL){
+        return true;
+    }
+    if(root->left == NULL){
+        return getDepth(root->right) == 1;
+    }
+    if(root->right == NULL){
+        return getDepth(root->left) == 1;
+    }
+    return getDepth(root->left) == getDepth(root->right) && equalPaths(root->left) && equalPaths(root->right);
+}
